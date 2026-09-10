3de5d29980a8a6009ae3693ad17ab4912b01498d
zeckyaem <zeckyaeliyamuhammad@gmail.com>
2026-09-10 19:37:11 +0700

    Add files via upload

---
+++ 0014_Zeckya_OperasiAritmatika_Komperasi.py
@@ -0,0 +1,17 @@
+p = 12
+l = 5 
+t = 8
+#Hitung Luas, Volume, Keliling
+luas = 2 * (p * l + p * t + l * t)
+volume = p * l * t
+keliling = 4 * ( p + l + t)
+
+print("Hasil Luas =", luas)
+print("Hasil volume =", volume)
+print("Hasil Keliling =", keliling)
+
+print("Apakah luas > 50?")
+print("Jawaban=", luas > 50)
+
+print("Apakah volume == 480?")
+print("Jawaban =", volume == 480)
